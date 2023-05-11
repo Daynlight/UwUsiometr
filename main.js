eb1654f8b2dc210e8d57c20672546903443a99bd
Daynlight <62157770+Daynlight@users.noreply.github.com>
2023-05-11 11:12:21 +0000

    xD

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 var Count = 0;
 var Wynik = 0;
 var AnimatioSpeed = 0.1;
+var NoweZdjęcie = true;
+
 
 function PrzeliczWynikTestu()
 {
   
   document.querySelector(".Przelicz").addEventListener("click",()=>
   {
+    if(NoweZdjęcie)
     Wynik = Math.floor(Math.random() * 100);
-   
+    NoweZdjęcie = false;
   })
 
 }
@@ -22,11 +25,19 @@ function WynikAnimation()
     if(Count>Wynik) Count -= AnimatioSpeed;
     bar.style.setProperty("--Wynik",`${Count}%`);
   }
-
+}
+function ZdjęciePoPrzeciągnięciu()
+{
   
-  
+  File = document.getElementById("file")
+  File.addEventListener("change",()=>
+  {
+    // document.getElementById("image").style.backgroundImage = `url(${File.data})`
+    NoweZdjęcie = true;
+  })
 }
 
 
 PrzeliczWynikTestu(); 
+ZdjęciePoPrzeciągnięciu()
 setInterval(WynikAnimation,1)
