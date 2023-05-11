601638bb6a350a1c9a43bf4a93fac902f319fc22
Daynlight <62157770+Daynlight@users.noreply.github.com>
2023-05-11 09:51:28 +0000

    Bar Random Number

---
+++ main.js
@@ -0,0 +1,31 @@
+var Count = 0;
+var Wynik = 0;
+var AnimatioSpeed = 0.1;
+
+function PrzeliczWynikTestu()
+{
+  
+  document.querySelector(".Przelicz").addEventListener("click",()=>
+  {
+    Wynik = Math.floor(Math.random() * 100);
+   
+  })
+
+}
+function WynikAnimation()
+{
+  var bar = document.getElementById("bar");
+
+  if((Wynik != Math.floor(Count)) && (Wynik != Math.round(Count)))
+  {
+    if(Count<Wynik) Count += AnimatioSpeed;
+    if(Count>Wynik) Count -= AnimatioSpeed;
+    bar.style.setProperty("--Wynik",`${Count}%`);
+  }
+  
+  
+}
+
+
+PrzeliczWynikTestu(); 
+setInterval(WynikAnimation,1)
